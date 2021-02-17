d9dfe416e7266d30304dfe46dea9cdde01f62e2d
Sasha <sasha@newterritory.studio>
2021-02-18 09:51:19 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var register = document.querySelector('.register'),
+registerBtn = document.querySelector('.register-header');
+
+registerBtn.addEventListener('click', () => {
+    register.classList.toggle('active');
+});
+
+console.log('hola');
