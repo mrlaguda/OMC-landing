4657439205011942204c236949b5ee62c058a7de
Sasha <sasha@newterritory.studio>
2021-02-18 18:56:27 +1100

    WIP

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 var register = document.querySelector('.register'),
-registerBtn = document.querySelector('.register-header');
+registerBtn = document.querySelector('.register-header'),
+textBody = document.querySelector('.text-body'),
+mobileHero = document.querySelector('.mobile-hero'),
+mobileBottom = document.querySelector('.mobile-bottom');
 
 registerBtn.addEventListener('click', () => {
-    register.classList.toggle('active');
+    
+    if(!register.classList.contains('active')) {
+        textBody.classList.add('fade');
+        mobileHero.classList.add('fade');
+        
+        setTimeout(() => {
+            mobileBottom.classList.add('hide');
+            mobileHero.classList.add('hide');
+            textBody.classList.add('remove');
+            register.classList.add('active');
+
+        }, 350);
+    }
+    if(register.classList.contains('active')) {
+        register.classList.remove('active');
+        setTimeout(() => {
+            textBody.classList.remove('remove');
+            mobileBottom.classList.remove('hide');
+            mobileHero.classList.remove('fade');
+            setTimeout(() => {
+                textBody.classList.remove('fade');
+                mobileHero.classList.remove('hide');
+            }, 10);
+        }, 350);
+    }
+
+    
+
+
 });
 
-console.log('hola');
